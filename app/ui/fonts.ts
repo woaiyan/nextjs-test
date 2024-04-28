798c5f8f0316e1e0b74c31a7a5602dee35de0cd0
caoguirong <2422400672@qq.com>
2024-04-28 21:17:32 +0800

    init

---
+++ app/ui/fonts.ts
@@ -0,0 +1,6 @@
+import {Inter, Oswald, Lusitana} from "next/font/google";
+
+export const inter = Inter({subsets: ["latin"]});
+export const oswald = Oswald({subsets: ["latin"]});
+
+export const lusitana = Lusitana({ weight: ['400', '700'], subsets: ['latin'],})
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,9 @@
-import type { Config } from 'tailwindcss';
-
-const config: Config = {
+module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx}',
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+    './src/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {
@@ -26,7 +25,5 @@ const config: Config = {
         },
       },
     },
-  },
-  plugins: [require('@tailwindcss/forms')],
+  }
 };
-export default config;
